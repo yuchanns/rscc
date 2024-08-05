b962839b91dc81e562ad7bec165c5e515e979393
yuchanns <me@yuchanns.xyz>
2024-08-05 21:22:16 +0800

    Add { ... }
    Signed-off-by: Hanchin Hsieh <me@yuchanns.xyz>

--- tests/test.rs
+++ tests/test.rs
@@ -32,45 +32,47 @@ fn run(input: &str) -> Result<Option<i32>> {
 fn test_compiler() -> Result<()> {
     assert!(Command::new("cargo").arg("build").status()?.success());
 
-    assert_eq!(run("return 0;")?, Some(0));
-    assert_eq!(run("return 42;")?, Some(42));
-    assert_eq!(run("return 5+20-4;")?, Some(21));
-    assert_eq!(run("return  12 + 34 - 5 ;")?, Some(41));
-    assert_eq!(run("return 5+6*7;")?, Some(47));
-    assert_eq!(run("return 5*(9-6);")?, Some(15));
-    assert_eq!(run("return (3+5)/2;")?, Some(4));
-    assert_eq!(run("return -10+20;")?, Some(10));
-    assert_eq!(run("return - -10;")?, Some(10));
-    assert_eq!(run("return - - +10;")?, Some(10));
+    assert_eq!(run("{ return 0; }")?, Some(0));
+    assert_eq!(run("{ return 42; }")?, Some(42));
+    assert_eq!(run("{ return 5+20-4; }")?, Some(21));
+    assert_eq!(run("{ return  12 + 34 - 5 ; }")?, Some(41));
+    assert_eq!(run("{ return 5+6*7; }")?, Some(47));
+    assert_eq!(run("{ return 5*(9-6); }")?, Some(15));
+    assert_eq!(run("{ return (3+5)/2; }")?, Some(4));
+    assert_eq!(run("{ return -10+20; }")?, Some(10));
+    assert_eq!(run("{ return - -10; }")?, Some(10));
+    assert_eq!(run("{ return - - +10; }")?, Some(10));
 
-    assert_eq!(run("return 0==1;")?, Some(0));
-    assert_eq!(run("return 42==42;")?, Some(1));
-    assert_eq!(run("return 0!=1;")?, Some(1));
-    assert_eq!(run("return 42!=42;")?, Some(0));
+    assert_eq!(run("{ return 0==1; }")?, Some(0));
+    assert_eq!(run("{ return 42==42; }")?, Some(1));
+    assert_eq!(run("{ return 0!=1; }")?, Some(1));
+    assert_eq!(run("{ return 42!=42; }")?, Some(0));
 
-    assert_eq!(run("return 0<1;")?, Some(1));
-    assert_eq!(run("return 1<1;")?, Some(0));
-    assert_eq!(run("return 2<1;")?, Some(0));
-    assert_eq!(run("return 0<=1;")?, Some(1));
-    assert_eq!(run("return 1<=1;")?, Some(1));
-    assert_eq!(run("return 2<=1;")?, Some(0));
+    assert_eq!(run("{ return 0<1; }")?, Some(1));
+    assert_eq!(run("{ return 1<1; }")?, Some(0));
+    assert_eq!(run("{ return 2<1; }")?, Some(0));
+    assert_eq!(run("{ return 0<=1; }")?, Some(1));
+    assert_eq!(run("{ return 1<=1; }")?, Some(1));
+    assert_eq!(run("{ return 2<=1; }")?, Some(0));
 
-    assert_eq!(run("return 1>0;")?, Some(1));
-    assert_eq!(run("return 1>1;")?, Some(0));
-    assert_eq!(run("return 1>2;")?, Some(0));
-    assert_eq!(run("return 1>=0;")?, Some(1));
-    assert_eq!(run("return 1>=1;")?, Some(1));
-    assert_eq!(run("return 1>=2;")?, Some(0));
+    assert_eq!(run("{ return 1>0; }")?, Some(1));
+    assert_eq!(run("{ return 1>1; }")?, Some(0));
+    assert_eq!(run("{ return 1>2; }")?, Some(0));
+    assert_eq!(run("{ return 1>=0; }")?, Some(1));
+    assert_eq!(run("{ return 1>=1; }")?, Some(1));
+    assert_eq!(run("{ return 1>=2; }")?, Some(0));
 
-    assert_eq!(run("a=3; return a;")?, Some(3));
-    assert_eq!(run("a=3; z=5; return a+z;")?, Some(8));
-    assert_eq!(run("a=b=3; return a+b;")?, Some(6));
-    assert_eq!(run("foo=3; return foo;")?, Some(3));
-    assert_eq!(run("foo123=3; bar=5; return foo123+bar;")?, Some(8));
+    assert_eq!(run("{ a=3; return a; }")?, Some(3));
+    assert_eq!(run("{ a=3; z=5; return a+z; }")?, Some(8));
+    assert_eq!(run("{ a=b=3; return a+b; }")?, Some(6));
+    assert_eq!(run("{ foo=3; return foo; }")?, Some(3));
+    assert_eq!(run("{ foo123=3; bar=5; return foo123+bar; }")?, Some(8));
 
-    assert_eq!(run("return 1; 2; 3;")?, Some(1));
-    assert_eq!(run("1; return 2; 3;")?, Some(2));
-    assert_eq!(run("1; 2; return 3;")?, Some(3));
+    assert_eq!(run("{ return 1; 2; 3; }")?, Some(1));
+    assert_eq!(run("{ 1; return 2; 3; }")?, Some(2));
+    assert_eq!(run("{ 1; 2; return 3; }")?, Some(3));
+
+    assert_eq!(run("{ {1; {2;} return 3;} }")?, Some(3));
 
     Ok(())
 }
